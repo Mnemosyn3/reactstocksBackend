52eba3f2f3d86c62e539b3f3de64a2d58d8c880e
Mnemosyn3 <robert.hamalainen@iki.fi>
2021-01-27 22:05:02 +0200

    Project is taking shape

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const http = require('http');
 const fetch = require('node-fetch');
 
 function getStockData(symbol){
+    let url = "https://www.reddit.com/r/popular.json";
+    let settings = { method: "Get" };
 
+    fetch(url, settings)
+        .then(res => res.json())
+        .then((json) => {
+            return json;
+    });
 }
 
 
